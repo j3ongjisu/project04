4a9d0efddf376693ef2faa23e5a05cd023ffbd5a
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-31 01:57:59 +0900

    Add files via upload

--- js/project04.js
+++ js/project04.js
@@ -1,11 +1,36 @@
 $(function () {
-  $('.main_slide').slick({
-      arrows: false,
-      fade: true,
+$('.main_slide').slick({
+    arrows: false,
+    fade: true,
       // vertical:true,
-      autoplay: true,
-      autoplaySpeed: 5000,
-      pauseOnHover: false,
-      pauseOnFocus: false,
-  });
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+});
+
+$(window).on('scroll', function () {
+    // 변수에 스크롤한 양을 담는다.
+    let sct = $(window).scrollTop();
+    console.log(sct)
+    if (sct > 0) {
+        $('.header').addClass('on')
+    } else {
+        $('.header').removeClass('on')
+    }
+
+});
+
+$('#bgndVideo').YTPlayer({
+    videoURL: 'https://youtu.be/R1mhOV0LoBE',
+    //재생하고 싶은 곳
+    containment: '#main_visual',
+    autoPlay: true,
+    showControls: false,
+    playOnlyIfVisible: true,
+
+});
+
+
+
 });
